8b6107ffc7fd142b4fbe12b3c7d0dc399308c9fb
silverhand7 <refojunior5@gmail.com>
2023-01-23 07:58:15 +0800

    kanban design

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
+    <div class="column">
+        <div class="column__header">
+            <div class="column__header__title">Title</div>
+            <div class="remove-button">X</div>
+        </div>
+        <div class="column__body">
+            <div class="card">
+                Lorem Ipsum Title
+            </div>
+            <div class="card">
+                Lorem Ipsum Title
+            </div>
+            <div class="card">
+                Lorem Ipsum Title
+            </div>
+        </div>
+        <div class="column__footer">
+            <button class="button">Add a card</button>
+        </div>
     </div>
+    <h1>Home Page</h1>
 </template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,6 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        @vite('resources/css/app.css')
-
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
