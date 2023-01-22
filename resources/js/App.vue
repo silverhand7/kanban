33cdcb703c164cab615a7be6612a4f60c77f2d0d
silverhand7 <refojunior5@gmail.com>
2023-01-22 20:17:28 +0800

    Sass

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="test">Test</router-link>
+  <div class="container">
+    <router-link to="/">Home</router-link> |
+    <router-link to="test">Test</router-link>
 
-  <router-view></router-view>
+    <router-view></router-view>
+  </div>
+
 </template>
+
+<style lang="scss">
+  @import "../css/app.scss";
+</style>
