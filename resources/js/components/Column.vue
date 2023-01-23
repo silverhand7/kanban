ae18056a91f4c9ae8a4c31ea1600b7af64be19fe
silverhand7 <refojunior5@gmail.com>
2023-01-23 11:41:15 +0800

    draggable

--- resources/js/components/Column.vue
+++ resources/js/components/Column.vue
@@ -1,30 +1,35 @@
 <template>
-    <div class="column">
-        <div class="column__header">
-            <div class="column__header__title">{{ title }}</div>
-            <div class="remove-button">X</div>
+    <Draggable :list="id" group="tasks">
+        <div class="column">
+            <div class="column__header">
+                <div class="column__header__title">{{ title }} </div>
+                <div class="remove-button">X</div>
+            </div>
+            <div class="column__body">
+                <CardItem v-for="item in items" :key="item.id">
+                    {{ item.title }}
+                </CardItem>
+            </div>
+            <div class="column__footer">
+                <button class="button">Add a card</button>
+            </div>
         </div>
-        <div class="column__body">
-            <CardItem v-for="item in items" :key="item.id">
-                {{ item.title }}
-            </CardItem>
-        </div>
-        <div class="column__footer">
-            <button class="button">Add a card</button>
-        </div>
-    </div>
+    </Draggable>
 </template>
 
 <script>
 import CardItem from './CardItem.vue';
+import Draggable from 'vuedraggable';
 
 export default {
     props: {
+        id: Number,
         title: String,
         items: Array
     },
     components: {
-        CardItem
+        CardItem,
+        Draggable
     }
 }
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="columns">
-        <Column v-for="column in columns" :key="column.id" :title="column.name" :items="column.cards" />
+        <Column v-for="column in columns" :key="column.id" :id="column.id" :title="column.name" :items="column.cards" />
     </div>
     <h1>Home Page</h1>
 </template>
@@ -16,7 +16,7 @@ export default {
         }
     },
     components: {
-        Column
+        Column,
     },
     async created() {
         await axios.get('/api/columns').then(response => (this.columns = response.data));
