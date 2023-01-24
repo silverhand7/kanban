0437a918876ba87a308503e64a39202e546467b6
silverhand7 <refojunior5@gmail.com>
2023-01-24 18:42:01 +0800

    download dump

--- app/Http/Controllers/ExportDBController.php
+++ app/Http/Controllers/ExportDBController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 class ExportDBController extends Controller
 {
     public function export() {
-        return \Spatie\DbDumper\Databases\PostgreSql::create()
+        \Spatie\DbDumper\Databases\PostgreSql::create()
         ->setDbName(env('DB_DATABASE'))
         ->setUserName(env('DB_USERNAME'))
         ->setPassword(env('DB_PASSWORD'))
         ->setHost(env('DB_HOST'))
         ->dumpToFile('dump.sql');
+
+        return response()->download('dump.sql');
     }
 }
